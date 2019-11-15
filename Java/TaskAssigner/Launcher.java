163c2724a9f28245729c8352040179c6b7a2be16
ivan <ivan.gonzalez.rincon@alumnos.uva.es>
2019-11-15 08:41:37 +0100

    Quitada carpeta

--- Java/TaskAssigner/Launcher.java
+++
@@ -1,9 +0,0 @@
-
-public class Launcher {
-	
-	public static void main(String[] args) {
-		TaskAssigner taskAssigner = new TaskAssigner("people.txt","tasks.txt","assign.txt");
-		taskAssigner.assign(taskAssigner.getTasks(), taskAssigner.getPeople());
-	}
-
-}
